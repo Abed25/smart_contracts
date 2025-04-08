9aeb2d4a38de1cf1a98bcefd82f76c25411f7180
Abed25 <officialwekesa254@gmail.com>
2025-04-08 16:29:27 +0300

    Added struct and array data structures

--- basics/age.sol
+++ basics/age.sol
@@ -4,11 +4,24 @@ pragma solidity ^0.8.19;
 contract welcomeSuperDev {
     uint private myAge = 22;
 
+struct UserDetails {
+    uint256 age;
+    string name;
+}
+
+UserDetails[] public listOfClients;
+
+//UserDetails public client = UserDetails({age: 20, name: "Musa"});
+
 function setYourAge(uint age)public {
     myAge = age;
 }
 
 function getYourAge() public view returns (uint){
     return myAge;
+}
+
+function addClients(uint256 _age, string memory _name) public{
+listOfClients.push(UserDetails( _age, _name));
 }
 }
